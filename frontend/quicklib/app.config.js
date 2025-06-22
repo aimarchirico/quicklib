202ece6c075d00837292d724e3ddcf64b691a2e2
Aimár A. Chirico <aimar@chirico.no>
2025-06-22 21:06:47 +0200

    feat(config): migrate app configuration from app.json to app.config.js

---
+++ frontend/quicklib/app.config.js
@@ -0,0 +1,50 @@
+export default {
+  expo: {
+    name: "QuickLib",
+    slug: "quicklib",
+    version: "1.0.0",
+    orientation: "portrait",
+    icon: "./assets/images/icon.png",
+    scheme: "quicklib",
+    userInterfaceStyle: "automatic",
+    newArchEnabled: true,
+    ios: {
+      supportsTablet: true
+    },
+    android: {
+      adaptiveIcon: {
+        foregroundImage: "./assets/images/adaptive-icon.png",
+        backgroundColor: "#ffffff"
+      },
+      edgeToEdgeEnabled: true,
+      package: "no.chirico.quicklib",
+      googleServicesFile: process.env.GOOGLE_SERVICES_JSON ?? './google-services.json'
+    },
+    web: {
+      bundler: "metro",
+      output: "static",
+      favicon: "./assets/images/favicon.png"
+    },
+    plugins: [
+      "expo-router",
+      [
+        "expo-splash-screen",
+        {
+          image: "./assets/images/splash-icon.png",
+          imageWidth: 200,
+          resizeMode: "contain",
+          backgroundColor: "#ffffff"
+        }
+      ]
+    ],
+    experiments: {
+      typedRoutes: true
+    },
+    extra: {
+      router: {},
+      eas: {
+        projectId: "e38c6fc6-6543-439c-9856-d091310dcd2a"
+      }
+    }
+  }
+};
