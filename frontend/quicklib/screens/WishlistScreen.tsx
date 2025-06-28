1d2a57f562a02b9a20d19f002ddbc43391b2e338
Aimár A. Chirico <aimar@chirico.no>
2025-06-28 18:38:34 +0200

    feat(screen): add WishlistScreen for displaying user's wishlist of books

---
+++ frontend/quicklib/screens/WishlistScreen.tsx
@@ -0,0 +1,38 @@
+import { BookResponseCollectionEnum } from '@/api/generated';
+import BookList from '@/components/BookList';
+import { Colors } from '@/globals/colors';
+import { useBooks } from '@/hooks/useBooks';
+import React from 'react';
+import { ActivityIndicator, StyleSheet, Text, View } from 'react-native';
+
+const WishlistScreen = () => {
+  const { books, loading, error } = useBooks(BookResponseCollectionEnum.Wishlist);
+
+  if (loading) {
+    return <ActivityIndicator size="large" color={Colors.brand.green} />;
+  }
+
+  if (error) {
+    return <Text style={styles.error}>{error}</Text>;
+  }
+
+  return (
+    <View style={styles.container}>
+      <BookList books={books} />
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: Colors.dark.background,
+  },
+  error: {
+    color: 'red',
+    textAlign: 'center',
+    marginTop: 20,
+  },
+});
+
+export default WishlistScreen;
