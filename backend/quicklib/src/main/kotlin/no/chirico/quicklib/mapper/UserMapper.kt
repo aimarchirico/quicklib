61f40cd6c53fb4b7ce8ad91883c10b3ecdfbc259
Aimár A. Chirico <aimar@chirico.no>
2025-06-28 03:18:09 +0200

    feat(mapper): add UserMapper interface for mapping UserRequest and UserResponse to/from UserEntity

---
+++ backend/quicklib/src/main/kotlin/no/chirico/quicklib/mapper/UserMapper.kt
@@ -0,0 +1,16 @@
+package no.chirico.quicklib.mapper
+
+import no.chirico.quicklib.dto.UserRequest
+import no.chirico.quicklib.dto.UserResponse
+import no.chirico.quicklib.entity.UserEntity
+import org.mapstruct.Mapper
+import org.mapstruct.MappingTarget
+
+@Mapper(componentModel = "spring")
+interface UserMapper {
+
+    fun toEntity(request: UserRequest): UserEntity
+    fun toDto(entity: UserEntity): UserResponse
+
+    fun updateEntity(request: UserRequest, @MappingTarget entity: UserEntity)
+}
