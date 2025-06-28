d0405034efe33eddc605d463854f0fc4d651f232
Aimár A. Chirico <aimar@chirico.no>
2025-06-28 18:29:46 +0200

    feat(dto): simplify UserRequest and UserResponse to use only firebaseUid

--- backend/quicklib/src/main/kotlin/no/chirico/quicklib/dto/UserDto.kt
+++ backend/quicklib/src/main/kotlin/no/chirico/quicklib/dto/UserDto.kt
@@ -1,12 +1,10 @@
 package no.chirico.quicklib.dto
 
 data class UserRequest(
-    val name: String,
-    val email: String
+    val firebaseUid: String
 )
 
 data class UserResponse(
     val id: Long,
-    val name: String,
-    val email: String
+    val firebaseUid: String
 )
