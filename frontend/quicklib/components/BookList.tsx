78bc6cd55b66fd965164f612f97f29a28c6327de
Aimár A. Chirico <aimar@chirico.no>
2025-06-29 21:23:33 +0200

    style(booklist): enhance BookList component with dynamic styling based on color scheme

--- frontend/quicklib/components/BookList.tsx
+++ frontend/quicklib/components/BookList.tsx
@@ -1,6 +1,8 @@
 import { BookResponse } from '@/api/generated';
 import BookListItem from '@/components/BookListItem';
-import React from 'react';
+import { Colors } from '@/globals/colors';
+import { useColorScheme } from '@/hooks/useColorScheme';
+import React, { useMemo } from 'react';
 import { FlatList, StyleSheet } from 'react-native';
 
 interface BookListProps {
@@ -8,21 +10,29 @@ interface BookListProps {
 }
 
 const BookList: React.FC<BookListProps> = ({ books }) => {
+  const colorScheme = useColorScheme();
+  const styles = useMemo(() => makeStyles(colorScheme), [colorScheme]);
+  
   return (
     <FlatList
       data={books}
       renderItem={({ item }) => <BookListItem book={item} />}
       keyExtractor={(item) => item.id.toString()}
       style={styles.list}
+      contentContainerStyle={styles.contentContainer}
+      showsVerticalScrollIndicator={false}
     />
   );
 };
 
-const styles = StyleSheet.create({
+const makeStyles = (colorScheme: 'light' | 'dark' | null) => StyleSheet.create({
   list: {
     flex: 1,
-    backgroundColor: '#151718',
+    backgroundColor: Colors[colorScheme ?? 'light'].background,
   },
+  contentContainer: {
+    paddingVertical: 0,
+  }
 });
 
 export default BookList;
