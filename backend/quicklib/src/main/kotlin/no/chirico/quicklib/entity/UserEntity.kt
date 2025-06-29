84ba2f365594eb350cc7edd268db5105f4f53092
Aimár A. Chirico <aimar@chirico.no>
2025-06-29 11:16:51 +0200

    refactor(entity): move id and createdAt properties to the body of UserEntity

--- backend/quicklib/src/main/kotlin/no/chirico/quicklib/entity/UserEntity.kt
+++ backend/quicklib/src/main/kotlin/no/chirico/quicklib/entity/UserEntity.kt
@@ -1,13 +1,20 @@
 package no.chirico.quicklib.entity
 
 import jakarta.persistence.*
+import java.time.Instant
+import org.hibernate.annotations.CreationTimestamp
 
 @Entity
 @Table(name = "users")
 class UserEntity(
-  @Id
-  @GeneratedValue(strategy = GenerationType.IDENTITY)
-  val id: Long? = null,
   @Column(unique = true, nullable = false)
   var firebaseUid: String
-)
+) {
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  val id: Long? = null
+
+  @Column(nullable = false, updatable = false)
+  @CreationTimestamp
+  val createdAt: Instant = Instant.now()
+}
