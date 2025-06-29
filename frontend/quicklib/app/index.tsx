70862608216078de90b84bf7ba94f3e6d0074417
Aimár A. Chirico <aimar@chirico.no>
2025-06-29 21:40:33 +0200

    refactor(Index): simplify index component by removing authentication logic and redirecting to login

--- frontend/quicklib/app/index.tsx
+++ frontend/quicklib/app/index.tsx
@@ -1,37 +1,6 @@
-import LoginButton from "@/components/LoginButton";
-import RevokeAccessButton from "@/components/RevokeAccessButton";
-import TestAuthButton from "@/components/TestAuthButton";
-import { FirebaseAuthTypes, getAuth, onAuthStateChanged } from "@react-native-firebase/auth";
 import { Redirect } from "expo-router";
-import React, { useEffect, useState } from "react";
-import { Text, View } from "react-native";
+import React from "react";
 
 export default function Index() {
-  const [user, setUser] = useState<FirebaseAuthTypes.User | null>(null);
-
-  useEffect(() => {
-    console.log("start")
-    const auth = getAuth();
-    const unsubscribe = onAuthStateChanged(auth, setUser);
-    return unsubscribe;
-  }, []);
-
-  if (user) {
-    return <Redirect href="/(tabs)" />;
-  }
-
-  return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Edit apps/index.tsx to edit this screen.</Text>
-      <TestAuthButton/>
-      <LoginButton user={user} />
-      <RevokeAccessButton />
-    </View>
-  );
+  return <Redirect href="/login" />;
 }
