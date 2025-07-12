dc9c6903241bc9a97339c0a47983d9a73ae1a050
Aimár A. Chirico <aimar@chirico.no>
2025-07-12 14:23:30 +0200

    feat(docker): add Docker image configuration

--- backend/quicklib/src/main/kotlin/no/chirico/quicklib/security/FirebaseConfig.kt
+++ backend/quicklib/src/main/kotlin/no/chirico/quicklib/security/FirebaseConfig.kt
@@ -5,13 +5,16 @@ import com.google.firebase.FirebaseOptions
 import com.google.auth.oauth2.GoogleCredentials
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import java.io.File
 import java.io.FileInputStream
 
 @Configuration
 class FirebaseConfig {
     @Bean
     fun firebaseApp(): FirebaseApp {
-        val serviceAccount = FileInputStream("src/main/resources/firebase-service-account.json")
+        // Firebase JSON is mounted from host via Docker volume
+        val serviceAccount = FileInputStream("/app/config/firebase-service-account.json")
+        
         val options = FirebaseOptions.builder()
             .setCredentials(GoogleCredentials.fromStream(serviceAccount))
             .build()
