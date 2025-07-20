cad0c814abc3ee6fe4c761eacc30da9c8b5b325b
Aimár A. Chirico <aimar@chirico.no>
2025-07-21 01:08:39 +0200

    refactor(repo): remove repository package

--- backend/quicklib/src/main/kotlin/no/chirico/quicklib/repository/BookRepository.kt
+++
@@ -1,9 +0,0 @@
-package no.chirico.quicklib.repository
-
-import no.chirico.quicklib.entity.BookEntity
-import no.chirico.quicklib.entity.UserEntity
-import org.springframework.data.jpa.repository.JpaRepository
-
-interface BookRepository : JpaRepository<BookEntity, Long> {
-    fun findAllByUser(user: UserEntity): List<BookEntity>
-}
--- backend/quicklib/src/main/kotlin/no/chirico/quicklib/repository/UserRepository.kt
+++
@@ -1,8 +0,0 @@
-package no.chirico.quicklib.repository
-
-import no.chirico.quicklib.entity.UserEntity
-import org.springframework.data.jpa.repository.JpaRepository
-
-interface UserRepository : JpaRepository<UserEntity, Long> {
-    fun findByFirebaseUid(firebaseUid: String): UserEntity?
-}
