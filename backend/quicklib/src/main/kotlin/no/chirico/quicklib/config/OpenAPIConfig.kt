0206b83d97690821cd5231908d6d3199f83a1409
Aimár A. Chirico <aimar@chirico.no>
2026-02-15 17:48:54 +0100

    chore(docker): upgrade to Eclipse Temurin 21 for build and runtime images

--- backend/quicklib/src/main/kotlin/no/chirico/quicklib/config/OpenAPIConfig.kt
+++ backend/quicklib/src/main/kotlin/no/chirico/quicklib/config/OpenAPIConfig.kt
@@ -1,7 +1,7 @@
 package no.chirico.quicklib.config
 
 import io.swagger.v3.oas.models.OpenAPI
-import io.swagger.v3.oas.models.servers.Server
+import io.swagger.v3.oas.models.info.Info
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
@@ -9,15 +9,15 @@ import org.springframework.context.annotation.Configuration
 @Configuration
 class OpenAPIConfig {
 
-    @Value("\${APP_URL}")
-    private lateinit var appUrl: String
-
     @Bean
-    fun customOpenAPI(): OpenAPI {
-        val server = Server()
-            .url("$appUrl/api")
-            .description("Server URL")
-        
-        return OpenAPI().servers(listOf(server))
+    fun customOpenAPI(
+        @Value("\${info.version}") version: String
+    ): OpenAPI {
+        return OpenAPI()
+            .info(
+                Info()
+                    .title("Quicklib API")
+                    .version(version)
+            )
     }
 }
